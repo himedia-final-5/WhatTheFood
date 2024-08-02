032f527dd029717dd18cda76615857a747f1f327
PresentKim(김현재) <debe3721@gmail.com>
2024-08-02 12:58:53 +0900

    feat: 서버 프로젝트 등록

---
+++ server/src/main/java/today/wtfood/server/WhatTheFoodApplication.java
@@ -0,0 +1,13 @@
+package today.wtfood.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTheFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatTheFoodApplication.class, args);
+    }
+
+}
