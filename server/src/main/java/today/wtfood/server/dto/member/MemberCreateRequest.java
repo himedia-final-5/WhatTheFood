33dbc9a29d973935dfb5c117ab2ea009548a7da2
PresentKim(김현재) <debe3721@gmail.com>
2024-08-05 11:12:20 +0900

    feat(member): 사용자 요청 DTO에 유효성 검사 추가

--- server/src/main/java/today/wtfood/server/dto/member/MemberCreateRequest.java
+++ server/src/main/java/today/wtfood/server/dto/member/MemberCreateRequest.java
@@ -1,20 +1,29 @@
 package today.wtfood.server.dto.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import today.wtfood.server.entity.Member;
 
 /**
  * 회원가입 요청 시 필요한 정보 DTO
- * TODO:  유효성 검사 추가
  *
  * @implNote DTO for {@link today.wtfood.server.entity.Member}
  */
 public record MemberCreateRequest(
+        @Size(min = 4, max = 45)
+        @Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "아이디는 영문, 숫자, _만 입력 가능합니다.")
         String username,
 
+        @Size(min = 4, max = 45)
         String password,
 
+        @Size(min = 2, max = 15)
+        @Pattern(regexp = "^[a-zA-Z0-9_가-힣]*$", message = "닉네임은 한글, 영문, 숫자, _만 입력 가능합니다.")
         String nickname,
 
+        @Size(min = 4, max = 45)
+        @Email
         String email
 ) {
 
--- server/src/main/java/today/wtfood/server/dto/member/MemberUpdateRequest.java
+++ server/src/main/java/today/wtfood/server/dto/member/MemberUpdateRequest.java
@@ -1,18 +1,27 @@
 package today.wtfood.server.dto.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 /**
  * 회원정보 변경 시 필요한 정보 DTO
- * TODO:  유효성 검사 추가
  *
  * @implNote DTO for {@link today.wtfood.server.entity.Member}
  */
 public record MemberUpdateRequest(
-        String nickname,
-
+        @Size(min = 4, max = 45)
         String password,
 
+        @Size(min = 2, max = 15)
+        @Pattern(regexp = "^[a-zA-Z0-9_가-힣]*$", message = "닉네임은 한글, 영문, 숫자, _만 입력 가능합니다.")
+        String nickname,
+
+        @Size(min = 4, max = 45)
+        @Email
         String email,
 
+        @Size(max = 200)
         String introduce
 ) {
 }
