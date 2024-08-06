f570cb8a98f449f0c72688ddfc5f6186616d16bb
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-08-06 22:04:23 +0900

    feat:(event)

--- client/src/Component/event/EventList.js
+++ client/src/Component/event/EventList.js
@@ -60,6 +60,7 @@ function EventList() {
     axios
       .get(`/api/events`,{params:{pageNumber: page}})
       .then((result) => {
+        //서버로 부터 페이지에 이어서 필요한 데이터를 전달 받고 기존 event 리스트에 추가함
         setEvents([...events, ...result.data.content]);
         setPageable(result.data);
       })
