29d15a6a70701409ee8d873354656d8784f1ff46
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-08-06 11:35:05 +0900

    feat:(event)

--- client/src/Component/event/EventList.js
+++ client/src/Component/event/EventList.js
@@ -7,32 +7,38 @@ import "../../style/Event.css";
 import { useDispatch } from "react-redux";
 
 function EventList() {
-  const [id, setId] = useState();
-  const [events, setEvents] = useState({ title: "", startDate: "", endDate: "", imageUrl: "", content: "" });
+  const [events, setEvents] = useState([]);
   const [imageUrl, setImageUrl] = useState([]);
   const [content, setContent] = useState([]);
+  const [paging,setPaging] = useState({});
 
   const dispath = useDispatch();
   const navigate = useNavigate();
 
   useEffect(
     ()=>{
-        jaxios.get(`/api/events/get/${id}`)
-        .then((result)=>{setId(result.data);})
+        jaxios.get(`/api/events`)
+        .then((result)=>{setEvents(result.data.content);})
         .catch((err)=>{console.error(err)})
-    },[id]
+    },[]
   );
 
   return (
     <div className="event_wrap">
-      <div className="event_state_wrap">
-        <span className="event_state_name">{events.title}</span>
-        <span className="event_date">
-          {events.startDate}&nbsp;&nbsp;{events.endDate}
-        </span>
-      </div>
-      <div className="event_imageUrl">{events.imageUrl}</div>
-      <span className="event_content">{events.content}</span>
+      {events.length > 0 ? (
+        events.map((event, index) => (
+          <div key={index} className="event_state_wrap">
+            <span className="event_state_name">{event.title}</span>
+            <span className="event_date">
+              {event.startDate}&nbsp;&nbsp;{event.endDate}
+            </span>
+            <div className="event_imageUrl">{event.imageUrl}</div>
+            <span className="event_content">{event.content}</span>
+          </div>
+        ))
+      ) : (
+        <div>No events found.</div>
+      )}
     </div>
   );
 }
