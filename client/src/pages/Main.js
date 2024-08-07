4493634f6af9685a5ed075f826368976c633865d
PresentKim(김현재) <debe3721@gmail.com>
2024-08-07 16:27:04 +0900

    refactor(client/main): CSS 파일 대신 tailwindcss를 통해 스타일 적용

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import './Main.css'
 
 import { useSelector, useDispatch } from 'react-redux';
 //import { getCookie } from '../utils/cookieUtil';
@@ -30,14 +29,14 @@ function Main() {
   return (
     <div>
       <Header setWord={setWord} />
-      <div className="Container">        
-          <div className='Center'>
+      <div className="flex w-full justify-center">        
+          <div className='w-5/6 text-center border border-red-500'>
 
                         {
                             (inquiryList)?(
                               inquiryList.map((inquirylist, idx)=>{
                                     return (
-                                        <div className='item' key={idx} onClick={()=>{ navigate(`/productDetail/${inquirylist.id}`) }}>
+                                        <div className='flex w-full justify-center' key={idx} onClick={()=>{ navigate(`/productDetail/${inquirylist.id}`) }}>
                                             <div className='name'>{inquirylist.id}</div>&nbsp;&nbsp;&nbsp;&nbsp;
                                             <div className='name'>{inquirylist.email}</div>&nbsp;&nbsp;&nbsp;&nbsp;
                                             <div className='name'>{inquirylist.title}</div>&nbsp;&nbsp;&nbsp;&nbsp;
