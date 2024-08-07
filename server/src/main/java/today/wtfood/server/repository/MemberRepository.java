1dc7ab422a5ae2bfd35e9de73e30a4ca8b5c1938
PresentKim(김현재) <debe3721@gmail.com>
2024-08-07 13:30:24 +0900

    fix(repository): 누락된 `@Repository` 어노테이션 추가

--- server/src/main/java/today/wtfood/server/repository/MemberRepository.java
+++ server/src/main/java/today/wtfood/server/repository/MemberRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 import today.wtfood.server.entity.Member;
 
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long>, JpaSpecificationExecutor<Member> {
 
     <T> Page<T> findAllBy(Pageable pageable, Class<T> projectionType);
