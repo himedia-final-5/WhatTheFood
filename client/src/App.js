2cbc4e1f01fe8cd11d9054ae57c2ba9df1547b8e
kkm41 <rpffkwns@naver.com>
2024-08-07 17:14:12 +0900

    refactor : 파일 이름 변경, 파일 삭제, 파일 삭제로 인한 코드 변경

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Main from './pages/Main';
 import JoinForm from './pages/member/JoinForm';
 import UpdateForm from './pages/member/UpdateForm';
 import WriteNotice from './pages/notice/WriteNotice';
-import NoticeList from './pages/notice/NoticeList';
+import Notice from './pages/notice/Notice';
 import EventList from './pages/event/EventList';
 import EventDetail from './pages/event/EventDetail';
 import Faq from './pages/inquiry/Faq';
@@ -18,7 +18,7 @@ function App() {
       <Route path="/login" element={<Login />}/>
       <Route path="/joinForm" element={<JoinForm />}/>
       <Route path="/updateForm" element={<UpdateForm />}/>
-      <Route path="/noticeList" element={<NoticeList />} />
+      <Route path="/notice" element={<Notice />} />
       <Route path="/writeNotice" element={<WriteNotice />} />
       <Route path="/events" element={<EventList />} />
       <Route path="/events/:id" element={<EventDetail />} />
