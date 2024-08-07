4ee30801a937131b21f1210058606c7ece532f31
PresentKim(김현재) <debe3721@gmail.com>
2024-08-07 11:37:22 +0900

    Revert "`Revert "Merge branch 'member' into event"`"
     This reverts commit a2ec0eadbd15f63947c1b6ca2383c935eb940527

--- server/src/main/resources/data/member.sql
+++ server/src/main/resources/data/member.sql
@@ -2,12 +2,13 @@
 SET SESSION FOREIGN_KEY_CHECKS = 0;
 
 -- 데이터 삽입
+-- $2a$10$QXmpZE9pcNXwzUNZQxMu7OuYeYTsJXveFoVvz77rR7R6vjaJgn5IC : 1111
 INSERT INTO member(username, nickname, email, password)
-VALUES ('user01', '홍길동', 'user01@wtfood.today', '1111'),
-       ('user02', '홍길순', 'user02@wtfood.today', '1111'),
-       ('user03', '김철수', 'user03@wtfood.today', '1111'),
-       ('user04', '나유리', 'user04@wtfood.today', '1111'),
-       ('user05', '신짱구', 'user05@wtfood.today', '1111');
+VALUES ('user01', '홍길동', 'user01@wtfood.today', '$2a$10$QXmpZE9pcNXwzUNZQxMu7OuYeYTsJXveFoVvz77rR7R6vjaJgn5IC'),
+       ('user02', '홍길순', 'user02@wtfood.today', '$2a$10$QXmpZE9pcNXwzUNZQxMu7OuYeYTsJXveFoVvz77rR7R6vjaJgn5IC'),
+       ('user03', '김철수', 'user03@wtfood.today', '$2a$10$QXmpZE9pcNXwzUNZQxMu7OuYeYTsJXveFoVvz77rR7R6vjaJgn5IC'),
+       ('user04', '나유리', 'user04@wtfood.today', '$2a$10$QXmpZE9pcNXwzUNZQxMu7OuYeYTsJXveFoVvz77rR7R6vjaJgn5IC'),
+       ('user05', '신짱구', 'user05@wtfood.today', '$2a$10$QXmpZE9pcNXwzUNZQxMu7OuYeYTsJXveFoVvz77rR7R6vjaJgn5IC');
 
 -- 외래키 제약조건을 다시 활성화
 SET SESSION FOREIGN_KEY_CHECKS = 1;
