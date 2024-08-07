b73036065edfc7ae852e5d3be35fbfdab5722288
PresentKim(김현재) <debe3721@gmail.com>
2024-08-07 12:19:06 +0900

    refactor(security): PasswordEncoder 빈을 등록하는 Configuration 을 분리

--- server/src/main/java/today/wtfood/server/security/SecurityConfig.java
+++ server/src/main/java/today/wtfood/server/security/SecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -63,11 +61,6 @@ public class SecurityConfig {
         );
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     @Bean
