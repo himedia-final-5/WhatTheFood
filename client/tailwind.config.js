a2535b1796bcd0f1d225e9e9a5f2c7ef5a129edf
PresentKim(김현재) <debe3721@gmail.com>
2024-08-09 04:49:19 +0900

    feat(client/config): TailwindCSS 애니메이션 함수 설정 기능 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,21 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "animate-ease": (value) => ({
+            animationTimingFunction: value,
+          }),
+        },
+        { values: theme("transitionTimingFunction") }
+      );
+    }),
+  ],
 };
