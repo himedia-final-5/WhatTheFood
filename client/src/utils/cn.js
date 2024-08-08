b6e9d640cf42d18531ef81d6383d5e1450011314
PresentKim(김현재) <debe3721@gmail.com>
2024-08-09 05:03:14 +0900

    feat(client): 주어진 클래스 이름을 tailwind에 알맞게 합쳐주는 유틸 함수(`cn) 추가

---
+++ client/src/utils/cn.js
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * 주어진 클래스 네임을 Tailwind CSS 클래스로 변환합니다.
+ *
+ * @type typeof clsx
+ */
+export const cn = (...inputs) => twMerge(clsx(inputs));
+export default cn;
