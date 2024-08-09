717a169a694e235ddcff9acf14e8b72317181573
PresentKim(김현재) <debe3721@gmail.com>
2024-08-09 20:48:45 +0900

    refactor(client/event): 이벤트 수정 페이지 리팩토링

--- client/src/pages/events/UpdateEvent.js
+++ client/src/pages/events/UpdateEvent.js
@@ -1,225 +1,165 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import axios from "../../utils/jwtUtil";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "./EventUpCreate.css";
+import ImageUploadInput from "../../components/util/ImageUploadInput";
 import { useSelector } from "../../stores";
+import useInputs from "../../hooks/useInputs";
+import cn from "../../utils/cn";
 
 function UpdateEvent() {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
   const loginUser = useSelector((state) => state.user);
+
+  const [event, setEvent] = useState({});
+  const { inputs, onInputChange } = useInputs(event);
+
   useEffect(() => {
     if (!loginUser) {
       alert("로그인이 필요합니다.");
       navigate("/login");
     }
-  }, [loginUser]);
-
-  const [pass, setPass] = useState("");
-  const [oldPass, setOldPass] = useState("");
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState("");
-
-  const [bannerImage, setBannerImage] = useState("");
-  const [contentImages, setContentImages] = useState("");
-
-  const [bannersavefilename, setBannerSaveFileName] = useState("");
-  const [contentsavefilename, setContentSaveFileName] = useState("");
-
-  const [bannerOldImgSrc, setBannerOldImgSrc] = useState("");
-  const [contentOldImgSrc, setContentOldImgSrc] = useState("");
-
-  const [imgStyle, setImgStyle] = useState({ display: "none" });
-
-  const [bannerimgSrc, setBannerImgSrc] = useState(
-    "http://via.placeholder.com/800x213"
-  );
-  const [contentimgSrc, setContentImgSrc] = useState(
-    "http://via.placeholder.com/800x2000"
-  );
-
-  const navigate = useNavigate();
-  const { id } = useParams();
+  }, [navigate, loginUser]);
 
   useEffect(() => {
     axios
-      .get(`/api/events/getEvent/${id}`)
-      .then((result) => {
-        setTitle(result.data.title);
-        setOldPass(result.data.pass);
-        setDate(result.data.data);
-
-        setBannerOldImgSrc(
-          `http://localhost:8070/image/${result.data.board.bannersavefilename}`
-        );
-        setBannerSaveFileName(result.data.bannersavefilename);
-
-        setContentOldImgSrc(
-          `http://localhost:8070/image/${result.data.board.contentsavefilename}`
-        );
-        setContentSaveFileName(result.data.contentsavefilename);
-
-        setBannerImage(result.data.bannerImage);
-        setContentImages(result.data.contentImages);
-
-        setBannerImgSrc(
-          `http://localhost:8070/images/${result.data.bannersavefilename}`
-        );
-        setContentImgSrc(
-          `http://localhost:8070/images/${result.data.contentsavefilename}`
-        );
-
-        setImgStyle({ width: "800px", display: "block" });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+      .get(`/api/events/${id}`)
+      .then((result) => setEvent(result.data))
+      .catch(console.error);
+  }, [id]);
 
   function onSubmit() {
-    if (oldPass != pass) {
-      return alert("수정 패스워드가 일치하지 않습니다");
-    }
     axios
-      .post("/api/events/updateEvent", {
-        id,
-        title,
-        date,
-        bannerImage,
-        contentImages,
-        bannersavefilename,
-        contentsavefilename,
-      })
-      .then(() => {
-        navigate(`/events/${id}`);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-
-  //FileUpload 수정해야됨!!
-  async function onFileUpload(e) {
-    const formData = new FormData();
-    formData.append("bannerImage", e.target.files[0]);
-    formData.append("contentImages", e.target.files[0]);
-    const result = await axios.post("/api/events/fileupload", formData);
-    setBannerSaveFileName(result.data.bannersavefilename);
-    setContentSaveFileName(result.data.contentsavefilename);
-    setBannerImage(result.data.bannerImage);
-    setContentImages(result.data.contentImages);
-
-    setBannerImgSrc(
-      `http://localhost:8070/images/${result.data.bannersavefilename}`
-    );
-    setContentImgSrc(
-      `http://localhost:8070/images/${result.data.contentsavefilename}`
-    );
-    setImgStyle({ width: "800px", display: "block" });
+      .post(`/api/events/${id}`, { ...event, ...inputs })
+      .then(() => navigate(`/events/${id}`))
+      .catch(console.error);
   }
 
   return (
     <div className="createEvent">
       <div className="createEvent_field">
         <label>작성자</label>
-        <input type="text" value={loginUser && loginUser.userid} readOnly />
-      </div>
-      {/* <div className='createEvent_field'>
-                <label>이메일</label><input type="text"  value={loginUser.email} readOnly/>
-            </div> */}
-      <div className="createEvent_field">
-        <label>PASS</label>
         <input
-          type="password"
-          value={pass}
-          onChange={(e) => {
-            setPass(e.currentTarget.value);
-          }}
+          type="text"
+          defaultValue={loginUser && loginUser.nickname}
+          readOnly
         />
       </div>
       <div className="createEvent_field">
         <label>제목</label>
         <input
           type="text"
-          value={title}
-          onChange={(e) => {
-            setTitle(e.currentTarget.value);
-          }}
+          name="title"
+          onChange={onInputChange}
+          defaultValue={event.title}
+          required
         />
       </div>
       <div className="createEvent_field">
-        <label>날짜</label>
+        <label htmlFor="startDate">시작 날짜</label>
         <input
-          type="text"
-          value={date}
-          onChange={(e) => {
-            setDate(e.currentTarget.value);
-          }}
+          type="date"
+          id="startDate"
+          name="startDate"
+          onChange={onInputChange}
+          defaultValue={event.startDate ? event.startDate.slice(0, 10) : ""}
+          required
         />
       </div>
       <div className="createEvent_field">
-        <label>배너 원래 이미지</label>
-        <div>
-          <img
-            src={bannerOldImgSrc}
-            style={{ width: "800px" }}
-            alt="banneroldImgSrc"
-          />
-        </div>
-      </div>
-      <div className="createEvent_field">
-        <label>배너 수정 이미지</label>
+        <label htmlFor="endDate">종료 날짜</label>
         <input
-          type="file"
-          onChange={(e) => {
-            onFileUpload(e);
-          }}
+          type="date"
+          id="endDate"
+          name="endDate"
+          onChange={onInputChange}
+          defaultValue={event.endDate ? event.endDate.slice(0, 10) : ""}
+          required
         />
-        {/* e 를 전달인수로 전달해야 해당함수에서 방금 선택한 이미지를 인식할 수 있습니다. */}
       </div>
       <div className="createEvent_field">
-        <div>
-          <img src={bannerimgSrc} style={imgStyle} alt="previewImg" />
-        </div>
-      </div>
-      <div className="createEvent_field">
-        <label>내용 원래 이미지</label>
-        <div>
-          <img
-            src={contentOldImgSrc}
-            style={{ width: "800px" }}
-            alt="contentoldImgSrc"
-          />
-        </div>
-      </div>
-      <div className="createEvent_field">
-        <label>내용 수정 이미지</label>
-        <input
-          type="file"
-          onChange={(e) => {
-            onFileUpload(e);
-          }}
+        <label>배너 이미지</label>
+        <ImageUploadInput
+          onUpload={(bannerImage) => setEvent({ ...event, bannerImage })}
+          imageSrc={event.bannerImage}
+          className={cn(
+            "flex flex-col items-center justify-center w-full overflow-hidden",
+            "border-2 border-gray-300 border-dashed rounded-lg"
+          )}
         />
-        {/* e 를 전달인수로 전달해야 해당함수에서 방금 선택한 이미지를 인식할 수 있습니다. */}
       </div>
       <div className="createEvent_field">
-        <div>
-          <img src={contentimgSrc} style={imgStyle} alt="previewImg" />
+        <label>컨텐츠 이미지 목록</label>
+        <div className="flex flex-wrap gap-y-2">
+          {event.contentImages &&
+            event.contentImages.length > 0 &&
+            event.contentImages.map((contentImage, index) => (
+              <div
+                key={index}
+                className={cn(
+                  "flex relative items-center justify-center w-full",
+                  "border-2 border-gray-300 border-dashed rounded-lg"
+                )}
+              >
+                <button
+                  aria-label={`remove-content-${index}`}
+                  className={cn(
+                    "absolute top-2 right-2 w-8 h-8 rounded-md transition-colors",
+                    "text-2xl text-red-500 hover:text-red-700",
+                    "bg-red-300 hover:bg-red-500"
+                  )}
+                  onClick={() =>
+                    setEvent({
+                      ...event,
+                      contentImages: event.contentImages.filter(
+                        (_, i) => i !== index
+                      ),
+                    })
+                  }
+                >
+                  X
+                </button>
+                <ImageUploadInput
+                  onUpload={(contentImage) =>
+                    setEvent({
+                      ...event,
+                      contentImages: event.contentImages.map((image, i) =>
+                        i === index ? contentImage : image
+                      ),
+                    })
+                  }
+                  imageSrc={contentImage}
+                  className={cn(
+                    "flex flex-col items-center justify-center w-full h-full overflow-hidden"
+                  )}
+                />
+              </div>
+            ))}
+          <button
+            onClick={() =>
+              setEvent({
+                ...event,
+                contentImages: [...(event.contentImages || []), ""],
+              })
+            }
+            className={cn(
+              "w-full py-2 rounded-md transition-colors",
+              "text-center items-center",
+              "text-2xl text-green-700 hover:text-green-200",
+              "bg-green-300 hover:bg-green-500"
+            )}
+          >
+            이미지 추가 +
+          </button>
         </div>
       </div>
       <div className="createEvent_btns">
-        <button
-          onClick={() => {
-            onSubmit();
-          }}
-        >
-          작성완료
-        </button>
-        <button
-          onClick={() => {
-            navigate("/events");
-          }}
-        >
-          돌아가기
-        </button>
+        <button onClick={onSubmit}>작성완료</button>
+        <Link to={`/events`}>
+          <button>돌아가기</button>
+        </Link>
       </div>
     </div>
   );
