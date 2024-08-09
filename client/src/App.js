8b863c8cf645537c7f6f3b3340d508b2887efe67
PresentKim(김현재) <debe3721@gmail.com>
2024-08-10 00:02:09 +0900

    style(client/router): App.js 라우터 코드 정리

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/member/Login";
-import Main from "./pages/main/Main";
-import JoinForm from "./pages/member/JoinForm";
-import UpdateForm from "./pages/member/UpdateForm";
-import WriteNotice from "./pages/notice/WriteNotice";
-import Notice from "./pages/notice/Notice";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
-import EventList from "./pages/event/EventList";
-import EventDetail from "./pages/event/EventDetail";
-import EventCreateBanner from "./pages/event/EventCreate";
-import UpdateEvent from "./pages/event/UpdateEvent";
-import Faq from "./pages/inquiry/Faq";
-import FaqView from "./pages/inquiry/FaqView";
-import FaqWriteForm from "./pages/inquiry/FaqWriteForm"; //임시
-import InquiryList from "./pages/inquiry/InquiryList"; // 임시
-import InquiryView from "./pages/inquiry/InquiryView"; // 임시
-import InquiryWriteForm from "./pages/inquiry/InquiryWriteForm"; //임시
-import Popup from "./pages/events/PopUp";
+
+import { Header, Footer } from "components/layout";
+import { Main } from "pages/main";
+import { MemberJoin, MemberLogin, MemberUpdate } from "pages/member";
+import { EventList, EventDetail, EventWrite, EventUpdate } from "pages/event";
+import { FaqList, FaqDetail, FaqWrite } from "pages/faq";
+import { InquiryList, InquiryDetail, InquiryWrite } from "pages/inquiry";
+import { NoticeList, NoticeDetail, NoticeWrite } from "pages/notice";
 
 function App() {
   return (
@@ -25,24 +14,27 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/joinForm" element={<JoinForm />} />
-        <Route path="/updateForm" element={<UpdateForm />} />
-        <Route path="/notice" element={<Notice />} />
-        <Route path="/writeNotice" element={<WriteNotice />} />
+
+        <Route path="/joinForm" element={<MemberJoin />} />
+        <Route path="/login" element={<MemberLogin />} />
+        <Route path="/setting" element={<MemberUpdate />} />
+
         <Route path="/events" element={<EventList />} />
         <Route path="/events/:id" element={<EventDetail />} />
-        <Route path="/createEventBanner" element={<EventCreateBanner />} />
-        <Route path="/updateEvent/:id" element={<UpdateEvent />} />
-        <Route path="/popUp" element={<Popup />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/faqView/:id" element={<FaqView />} />
+        <Route path="/createEventBanner" element={<EventWrite />} />
+        <Route path="/updateEvent/:id" element={<EventUpdate />} />
 
-        {/* 임시*/}
+        <Route path="/faq" element={<FaqList />} />
+        <Route path="/faqView/:id" element={<FaqDetail />} />
+        <Route path="/faqWriteForm" element={<FaqWrite />} />
+
         <Route path="/inquiryList" element={<InquiryList />} />
-        <Route path="/inquiryView/:id" element={<InquiryView />} />
-        <Route path="/inquiryWriteForm" element={<InquiryWriteForm />} />
-        <Route path="/faqWriteForm" element={<FaqWriteForm />} />
+        <Route path="/inquiryView/:id" element={<InquiryDetail />} />
+        <Route path="/inquiryWriteForm" element={<InquiryWrite />} />
+
+        <Route path="/notice" element={<NoticeList />} />
+        <Route path="/notice/:id" element={<NoticeDetail />} />
+        <Route path="/writeNotice" element={<NoticeWrite />} />
       </Routes>
       <Footer />
     </div>
