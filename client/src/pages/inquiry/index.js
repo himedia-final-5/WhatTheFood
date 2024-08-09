3878181f27995449ba6e8886ab1b1adabb19c174
PresentKim(김현재) <debe3721@gmail.com>
2024-08-09 23:54:18 +0900

    refactor(client): faq과 inquiry 페이지 컴포넌트 디렉토리 분리

--- client/src/pages/inquiry/index.js
+++ client/src/pages/inquiry/index.js
@@ -1,7 +1,3 @@
-export { default as FaqList } from "./Faq";
-export { default as FaqDetail } from "./FaqView";
-export { default as FaqWrite } from "./FaqWriteForm";
-
 export { default as InquiryList } from "./InquiryList";
 export { default as InquiryDetail } from "./InquiryView";
 export { default as InquiryWrite } from "./InquiryWriteForm";
