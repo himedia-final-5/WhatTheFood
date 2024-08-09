81d58d39122a54757acbb9cead537fb2441adaec
PresentKim(김현재) <debe3721@gmail.com>
2024-08-09 23:40:41 +0900

    feat(client/component): 각 컴포넌트 그룹 별로 index.js 파일을 통해 한번에 export

---
+++ client/src/components/asset/index.js
@@ -0,0 +1,3 @@
+export { default as CloseMark } from "./CloseMark.jsx";
+
+export { default as UndrawEatingTogether } from "./UndrawEatingTogether.jsx";
---
+++ client/src/components/util/index.js
@@ -0,0 +1,2 @@
+export { default as ImageUploadInput } from "./ImageUploadInput";
+export { default as Pagination } from "./Pagination";
