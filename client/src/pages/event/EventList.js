8a39ef72daf9da51b0de4f846f5eae22722bb673
PresentKim(김현재) <debe3721@gmail.com>
2024-08-10 11:57:15 +0900

    refactor(client): `useInfiniteScroll` 훅을 사용해 간단하게 무한 스크롤 구현

--- client/src/pages/event/EventList.js
+++ client/src/pages/event/EventList.js
@@ -1,68 +1,19 @@
-import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./EventList.css";
 import { axios } from "utils";
-import { usePageResponse } from "hooks";
-
-/** @type EventSummary[] */
-const DEFAULT_CONTENT = [];
-const SCROLL_DATA_COUNT = 4;
+import { useInfiniteScroll } from "hooks";
 
 function EventList() {
   const navigate = useNavigate();
-  const { content, pagination, setPageResponse } =
-    usePageResponse(DEFAULT_CONTENT);
-  const [accumulatedContent, setAccumulatedContent] = useState(DEFAULT_CONTENT);
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(
-    () => setAccumulatedContent((prevPosts) => [...prevPosts, ...content]),
-    [content],
-  );
+  const { ref, content } = useInfiniteScroll(async (page) => {
+    /** @type {{data: PageResponse<EventSummary>}} */
+    const response = await axios.get(`/api/events`, {
+      params: { page, size: 8 },
+    });
 
-  const handleScroll = () => {
-    const scrollHeight = document.documentElement.scrollHeight - 10; // 스크롤이 가능한 크기
-    const scrollTop = document.documentElement.scrollTop; // 현재 위치
-    const clientHeight = document.documentElement.clientHeight; // 내용물의 크기
-
-    if (scrollTop + clientHeight >= scrollHeight) {
-      loadPage(Number(pagination.page) + 1);
-    }
-  };
-
-  function onEventView(id) {
-    navigate(`/events/${id}`);
-  }
-
-  //무한스크롤
-  function loadPage(page) {
-    // 마지막 페이지거나, 이미 다음 페이지 요청이 시작된 경우 무시
-    if (pagination.last || isLoading) {
-      return;
-    }
-
-    // 페이지 요청
-    setIsLoading(true);
-    axios
-      .get(`/api/events`, { params: { page, size: SCROLL_DATA_COUNT } })
-      .then((result) => setPageResponse(result.data))
-      .catch(console.error)
-      .finally(() => setIsLoading(false));
-  }
-
-  useEffect(() => {
-    // 이벤트 목록이 비어있으면 첫번째 페이지 데이터 요청
-    if (content.length === 0) {
-      loadPage(0);
-    }
-  });
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return response.data;
   });
 
   return (
@@ -75,14 +26,12 @@ function EventList() {
         게시글쓰기
       </button>
       &nbsp;&nbsp;&nbsp;
-      {accumulatedContent.length > 0 ? (
-        accumulatedContent.map((event, index) => (
+      {content.length > 0 ? (
+        content.map((event, index) => (
           <div
             key={index}
             className="event_state_wrap"
-            onClick={() => {
-              onEventView(event.id);
-            }}
+            onClick={() => navigate(`/events/${event.id}`)}
           >
             <div className="event_text_wrap">
               <span className="event_state_name">{event.title}</span>
@@ -100,6 +49,7 @@ function EventList() {
       ) : (
         <div>No events found.</div>
       )}
+      <div aria-label="scroll-trigger" ref={ref} />
     </div>
   );
 }
