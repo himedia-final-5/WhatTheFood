2b43bb066ffe9b28fd7a08c29e756334e6f06e50
PresentKim(김현재) <debe3721@gmail.com>
2024-08-10 11:16:17 +0900

    style(client/util): `cn` 유틸을 함수 형식으로 변경

--- client/src/utils/cn.js
+++ client/src/utils/cn.js
@@ -4,7 +4,9 @@ import { twMerge } from "tailwind-merge";
 /**
  * 주어진 클래스 네임을 Tailwind CSS 클래스로 변환합니다.
  *
- * @type typeof clsx
+ * @param {ClassValue[]} inputs
+ * @returns {string}
  */
-export const cn = (...inputs) => twMerge(clsx(inputs));
-export default cn;
+export default function cn(...inputs) {
+  return twMerge(clsx(inputs));
+}
