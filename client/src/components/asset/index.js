e332fd7673157d7221141bf2f000c94cf53160ea
PresentKim(김현재) <debe3721@gmail.com>
2024-08-10 23:00:48 +0900

    feat(client/component): 아이콘으로 사용하기 위해 Tabler 아이콘 컴포넌트 2개 추가

--- client/src/components/asset/index.js
+++ client/src/components/asset/index.js
@@ -1,3 +1,5 @@
 export { default as CloseMark } from "./CloseMark.jsx";
+export { default as TablerCircleKeyFilled } from "./TablerCircleKeyFilled";
+export { default as TablerUserFilled } from "./TablerUserFilled";
 
 export { default as UndrawEatingTogether } from "./UndrawEatingTogether.jsx";
