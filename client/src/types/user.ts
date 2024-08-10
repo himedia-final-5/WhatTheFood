9ba2626e65441827fc5896a9c23058ad08379d90
PresentKim(김현재) <debe3721@gmail.com>
2024-08-10 23:15:38 +0900

    fix(client/type): User 타입의 누락된 `role` 필드를 추가

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,7 +1,15 @@
+declare type Role =
+  | "ROLE_GHEST"
+  | "ROLE_USER"
+  | "ROLE_CHEF"
+  | "ROLE_BRAND"
+  | "ROLE_ADMIN";
+
 declare type User = {
   id: number;
   username: string;
   nickname: string;
+  role: Role;
   accessToken: string;
   refreshToken: string;
 };
