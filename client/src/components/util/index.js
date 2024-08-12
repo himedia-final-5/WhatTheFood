683490cab5147b605c482d19ef5e20b2ad93bc0e
PresentKim(김현재) <debe3721@gmail.com>
2024-08-12 12:45:03 +0900

    feat(client): 관리자 여부에 따라 다르게 표시하는 `AdminFeature`, `AdminFeatureContainer` 컴포넌트 구현

--- client/src/components/util/index.js
+++ client/src/components/util/index.js
@@ -1,3 +1,5 @@
+export { default as AdminFeature } from "./AdminFeature";
+export { default as AdminFeatureContainer } from "./AdminFeatureContainer";
 export { default as ImageUploadInput } from "./ImageUploadInput";
 export { default as PaginationNav } from "./PaginationNav";
 export { default as Modal } from "./Modal";
