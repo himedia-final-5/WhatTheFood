d133a7a18875eeb083b985722db0efe54b5a1822
PresentKim(김현재) <debe3721@gmail.com>
2024-08-12 17:57:08 +0900

    feat(client/hook): 프로미스 진행 상태와 오류 여부를 상태로 반환하는 `usePromise` 훅 추가

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,5 +1,6 @@
 export { default as useInputs } from "./useInputs";
 export { default as usePageResponse } from "./usePageResponse";
 export { default as useInfiniteScroll } from "./useInfiniteScroll";
+export { default as usePromise } from "./usePromise";
 export { default as usePromiseThrottle } from "./usePromiseThrottle";
 export { default as useToggleElement } from "./useToggleElement";
