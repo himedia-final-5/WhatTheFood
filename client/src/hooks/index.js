7c742d1f055883735c5454b3d7a9bb03cabd84b8
PresentKim(김현재) <debe3721@gmail.com>
2024-08-12 16:08:42 +0900

    feat(client): 프로미스 실행의 반복 시간을 제한하는 데에 사용하는 `usePromiseThrottle` 훅 구현

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useInputs } from "./useInputs";
 export { default as usePageResponse } from "./usePageResponse";
 export { default as useInfiniteScroll } from "./useInfiniteScroll";
+export { default as usePromiseThrottle } from "./usePromiseThrottle";
 export { default as useToggleElement } from "./useToggleElement";
