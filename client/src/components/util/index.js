cd6029e3bb0e2640ff155f9fae26723b8478794b
PresentKim(김현재) <debe3721@gmail.com>
2024-08-12 12:53:56 +0900

    feat(client): 로그인 여부에 따라 다르게 표시하는 `UserFeature` 관련 컴포넌트 구현

--- client/src/components/util/index.js
+++ client/src/components/util/index.js
@@ -1,5 +1,8 @@
 export { default as AdminFeature } from "./AdminFeature";
 export { default as AdminFeatureContainer } from "./AdminFeatureContainer";
+export { default as UserFeature } from "./UserFeature";
+export { default as UserFeatureContainer } from "./UserFeatureContainer";
+
 export { default as ImageUploadInput } from "./ImageUploadInput";
 export { default as PaginationNav } from "./PaginationNav";
 export { default as Modal } from "./Modal";
