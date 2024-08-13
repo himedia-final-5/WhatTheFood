63e8fa6d3f630a50de01c00ff6b8a7bebd29636b
PresentKim(김현재) <debe3721@gmail.com>
2024-08-13 17:50:32 +0900

    refactor(client): AuthModal의 세부 폼을 하위 디렉토리로 정리

--- client/src/components/modal/auth/index.js
+++ client/src/components/modal/auth/index.js
@@ -1,5 +1,5 @@
 import { lazyLoading as $ } from "utils";
 
 export const AuthModal = $(() => import("./AuthModal"));
-export const AuthSignInForm = $(() => import("./AuthSignInForm"));
-export const AuthSignUpForm = $(() => import("./AuthSignUpForm"));
+export const AuthSignInForm = $(() => import("./signin/AuthSignInForm"));
+export const AuthSignUpForm = $(() => import("./signup/AuthSignUpForm"));
