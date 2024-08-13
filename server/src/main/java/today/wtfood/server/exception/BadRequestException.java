d380af5d008377fa3c0f5cdf47efa7a65bffd7d5
PresentKim(김현재) <debe3721@gmail.com>
2024-08-14 00:21:38 +0900

    feat(server/exception): BAD_REQUEST 처리 시 필드 이름을 함께 반환할 수 있도록 필드 추가

--- server/src/main/java/today/wtfood/server/exception/BadRequestException.java
+++ server/src/main/java/today/wtfood/server/exception/BadRequestException.java
@@ -5,8 +5,18 @@ import org.springframework.web.server.ResponseStatusException;
 
 public class BadRequestException extends ResponseStatusException {
 
+    /**
+     * 필드 이름
+     */
+    private String field;
+
     public BadRequestException(String message) {
+        this(message, null);
+    }
+
+    public BadRequestException(String message, String field) {
         super(HttpStatus.BAD_REQUEST, message);
+        this.field = field;
     }
 
 }
