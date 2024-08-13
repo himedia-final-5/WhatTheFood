1ea64035778afa88cad5b563b6f3839ae5ad6594
PresentKim(김현재) <debe3721@gmail.com>
2024-08-13 10:02:34 +0900

    feat(client/util): 지연 로딩 컴포넌트를 `<Suspense>` 컴포넌트로 감싸서 반환하는 `lazyLoading` 구현

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,3 +2,4 @@ export { default as cn } from "./cn";
 export { default as defaultErrorHandler } from "./defaultErrorHandler";
 export { default as axios } from "./jwtUtil";
 export { default as initialPagination } from "./initialPagination";
+export { default as lazyLoading } from "./lazyLoading";
