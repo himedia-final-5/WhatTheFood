aff52eb40881b002dcb97cafdc4fe45d805e658b
PresentKim(김현재) <debe3721@gmail.com>
2024-08-14 15:26:21 +0900

    refactor(server/config): ConfigurationProperties 을 사용해 이메일 관련 설정을 빈에 바인딩

--- server/src/main/java/today/wtfood/server/WhatTheFoodApplication.java
+++ server/src/main/java/today/wtfood/server/WhatTheFoodApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import today.wtfood.server.config.properties.JwtProperties;
+import today.wtfood.server.config.properties.MailProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         JwtProperties.class,
+        MailProperties.class
 })
 public class WhatTheFoodApplication {
 
