59f8ee1b2771f82d64dad690c2bbc8784a66b242
kkm41 <rpffkwns@naver.com>
2024-08-14 18:34:23 +0900

    feat: 랭킹 목록 틀만 추가

--- client/src/pages/ranking/RankingList.js
+++ client/src/pages/ranking/RankingList.js
@@ -1,14 +1,33 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { FaStar, FaRegStar } from "react-icons/fa";
 import "./css.css";
-import { AdminFeature } from "components/util";
+
 import { axios, defaultErrorHandler } from "utils";
 import { useInfiniteScroll, usePromiseThrottle } from "hooks";
 
-export default function EventList() {
+export default function RankingList() {
+  const starStyle = {
+    color: "#FFB400",
+    fontSize: "15px",
+  };
+
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (query) => {
+    navigate(`/recipes?category=${query}`);
+  };
+
   const [throttleInterval, setThrottleInterval] = useState(0);
   const throttle = usePromiseThrottle(throttleInterval);
+  const categories = [
+    { name: "이탈리안", query: "italian" },
+    { name: "중식", query: "chinese" },
+    { name: "멕시칸", query: "mexican" },
+    { name: "인도식", query: "indian" },
+    { name: "한식", query: "korean" },
+    { name: "일식", query: "japanese" },
+  ];
   const { ref, content } = useInfiniteScroll(
     throttle(async (page) => {
       /** @type {{data: PageResponse<EventSummary>}} */
@@ -25,48 +44,47 @@ export default function EventList() {
   );
 
   return (
-    <div className="event_banner_wrap relative">
-      <ul>
-        <AdminFeature>
-          <Link
-            to="/events/write"
-            className="absolute left-6 -top-5 px-2 py-0.5 "
-          >
-            레시피
-          </Link>
-          <Link
-            to="/events/write"
-            className="absolute left-24 -top-5 px-2 py-0.5 "
-          >
-            검색어
-          </Link>
-          <Link
-            to="/events/write"
-            className="absolute left-40 -top-5 px-2 py-0.5 "
-          >
-            쉐프
-          </Link>
-        </AdminFeature>
-      </ul>
+    <div className="ranking_banner_wrap relative">
+      <div className="absolute -top-28">
+        <ul className="category-list">
+          {categories.map((category, index) => (
+            <li key={index} className="category-item">
+              <button
+                onClick={() => handleCategoryClick(category.query)}
+                className="category-button"
+              >
+                {category.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="absolute flex space-x-16 -top-16 left-96 text-lg "></div>
+      <div className="absolute flex right-80 top-4 text-lg ">
+        <p className="border py-1 px-5">일간</p>
+        <p className="border py-1 px-5">주간</p>
+        <p className="border py-1 px-5">월간</p>
+        <FaStar style={starStyle} />
+        <FaRegStar style={starStyle} />
+      </div>
+
       {content.length > 0 ? (
         content.map((event, index) => (
-          <Link
-            to={`/events/${event.id}`}
-            key={index}
-            className="event_state_wrap"
-          >
-            <div className="event_imageUrl">
-              <img src={event.bannerImage} alt="event_bannerImage"></img>
+          <div key={index} className="ranking_container">
+            <p className="ranking_num">
+              <b>{index + 1}</b>
+            </p>
+            <div className="ranking_state_wrap">
+              <Link to={`/events/${event.id}`}>
+                <div className="ranking_imageUrl">
+                  <img src={event.bannerImage} alt="event_bannerImage" />
+                </div>
+              </Link>
+              <div className="ranking_text_below">
+                <p>{event.title}</p>
+              </div>
             </div>
-
-            <div className="event_text_wrap">
-              <span className="event_state_name">{event.title}</span>
-              <span className="event_date">
-                {event.startDate.slice(0, 10)}&nbsp;&nbsp;
-                {event.endDate && event.endDate.slice(0, 10)}
-              </span>
-            </div>
-          </Link>
+          </div>
         ))
       ) : (
         <div>No events found.</div>
