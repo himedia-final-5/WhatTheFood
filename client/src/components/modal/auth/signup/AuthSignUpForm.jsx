69f72bc8a378bf3e799b0e416bddab04a16f82da
PresentKim(김현재) <debe3721@gmail.com>
2024-08-14 15:38:12 +0900

    feat(client/auth): 회원 가입 폼에 인증 메일 전송 기능 추가

--- client/src/components/modal/auth/signup/AuthSignUpForm.jsx
+++ client/src/components/modal/auth/signup/AuthSignUpForm.jsx
@@ -1,88 +1,138 @@
 import { toast } from "react-toastify";
 
-import { TablerCircleKeyFilled, TablerUserFilled } from "components/asset";
-import { cn } from "utils";
-import { useInputs } from "hooks";
+import { TablerCircleKeyFilled, TablerMailFilled } from "components/asset";
+import { cn, axios } from "utils";
+import useInput from "hooks/useInput";
+import usePromise from "hooks/usePromise";
+import Input from "components/form/Input";
 
 export default function AuthSignUpForm() {
-  const { inputs, onInputChange } = useInputs({});
+  const [email, onEmailInputChange] = useInput("");
+  const [token, onTokenInputChange] = useInput("");
 
-  async function onFormSubmit() {
-    /** TODO: 회원가입 기능 구현 */
-    return toast.warn("회원가입은 준비 중입니다.");
+  const [sendEmail, emailSent, isEmailSendingInProgress] = usePromise(
+    false,
+    async () =>
+      (
+        await axios.post("/api/auth/signup/verify-email", null, {
+          params: { email },
+        })
+      )?.status === 204,
+  );
+
+  async function onRequireSendMail() {
+    if (emailSent || isEmailSendingInProgress) {
+      toast.warn("이메일이 이미 전송되었거나 전송 중입니다.");
+      return;
+    }
+    toast.promise(sendEmail(), {
+      pending: "회원가입 메일을 전송 중입니다.",
+      success: "이메일이 전송되었습니다. 확인해주세요.",
+      error: {
+        render({ data: error }) {
+          return error?.toastMessage || "이메일 전송에 실패했습니다.";
+        },
+      },
+    });
   }
 
   return (
-    <form
-      aria-label="auth-input-form"
-      className="flex w-full h-fit"
-      onSubmit={(event) => {
-        event.preventDefault();
-        onFormSubmit();
-      }}
+    <div
+      className={cn(
+        "w-full h-fit transition-shape",
+        emailSent ? "mb-0" : "mb-12",
+      )}
     >
-      <div className="flex flex-col flex-1">
-        <div aria-label="auth-input-username" className="w-full h-12 flex">
-          <label htmlFor="username" className="flex">
-            <span className="flex items-center px-3 bg-neutral-50 border border-solid border-e-0 border-gray-300 rounded-ss-md">
-              <TablerUserFilled className="w-6 h-8 text-neutral-900 text-opacity-50" />
-            </span>
-          </label>
-          <input
-            id="username"
-            name="username"
-            type="text"
-            autoComplete="username"
-            autoCorrect="off"
-            placeholder="아이디"
-            defaultValue={inputs.username}
-            onChange={onInputChange}
-            required
-            className={cn(
-              "block flex-1 min-w-0 w-full p-2.5",
-              "bg-gray-50 border border-solid border-gray-300",
-              "text-gray-900 text-base focus:border-green-500",
-            )}
-          />
-        </div>
-        <div aria-label="auth-input-password" className="w-full h-12 flex">
-          <label htmlFor="password" className="flex">
-            <span className="flex items-center px-3 bg-neutral-50 border border-solid border-e-0 border-gray-300 rounded-es-md">
-              <TablerCircleKeyFilled className="w-6 h-8 text-neutral-900 text-opacity-50" />
-            </span>
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="password"
-            autoCorrect="off"
-            placeholder="비밀번호"
-            defaultValue={inputs.password}
-            onChange={onInputChange}
-            required
-            className={cn(
-              "block flex-1 min-w-0 w-full p-2.5",
-              "bg-gray-50 border border-solid border-gray-300",
-              "text-gray-900 text-base focus:border-green-500",
-            )}
-          />
-        </div>
-      </div>
-      <div
-        aria-label="auth-submit-button"
-        className="w-16 h-full bg-green-500 rounded-e-md"
+      <form
+        aria-label="auth-input-form"
+        className="flex w-full h-full"
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          // 이메일이 전송되지 않았을 때 이메일 전송 요청
+          if (!emailSent && !isEmailSendingInProgress) {
+            onRequireSendMail();
+          }
+        }}
       >
-        <button
-          type="submit"
-          className={cn(
-            "w-full h-full",
-            "text-white text-base font-bold drop-shadow",
-          )}
+        <div className="flex flex-col flex-1">
+          <div aria-label="auth-input-email" className="flex w-full h-12 z-20">
+            <Input
+              name="email"
+              labelProps={{
+                className: cn(
+                  "flex items-center px-3 transition-shape",
+                  "bg-neutral-50 border border-solid border-e-0 border-gray-300",
+                  emailSent ? "rounded-ss-md" : "rounded-l-md",
+                ),
+              }}
+              type={emailSent || isEmailSendingInProgress ? "none" : "email"}
+              autoComplete={
+                emailSent || isEmailSendingInProgress ? "none" : "email"
+              }
+              autoCorrect="off"
+              placeholder="회원가입할 이메일"
+              onChange={onEmailInputChange}
+              readOnly={emailSent || isEmailSendingInProgress}
+              disabled={emailSent || isEmailSendingInProgress}
+              required
+              className={cn(
+                "block flex-1 min-w-0 w-full p-2.5",
+                "bg-gray-50 border border-solid border-gray-300",
+                "text-gray-900 text-base focus:border-green-500",
+              )}
+            >
+              <TablerMailFilled className="w-6 h-8 text-neutral-900 text-opacity-50" />
+            </Input>
+          </div>
+          <div
+            aria-label="auth-input-token"
+            className={cn(
+              "flex w-full h-12 transition-shape z-10",
+              emailSent ? "mt-0" : "-mt-12",
+            )}
+          >
+            <Input
+              name="token"
+              labelProps={{
+                className: cn(
+                  "flex items-center px-3 transition-shape",
+                  "bg-neutral-50 border border-solid border-e-0 border-gray-300",
+                  emailSent ? "rounded-es-md" : "rounded-l-md",
+                ),
+              }}
+              type="token"
+              autoComplete="none"
+              autoCorrect="off"
+              placeholder="메일로 전송된 토큰"
+              onChange={onTokenInputChange}
+              required
+              readOnly={!emailSent}
+              className={cn(
+                "block flex-1 min-w-0 w-full p-2.5",
+                "bg-gray-50 border border-solid border-gray-300",
+                "text-gray-900 text-base focus:border-green-500",
+              )}
+            >
+              <TablerCircleKeyFilled className="w-6 h-8 text-neutral-900 text-opacity-50" />
+            </Input>
+          </div>
+        </div>
+        <div
+          aria-label="auth-submit-button"
+          className="w-16 bg-green-500 rounded-e-md"
         >
-          회원가입
-        </button>
-      </div>
-    </form>
+          <button
+            type="submit"
+            className={cn(
+              "w-full h-full",
+              "text-white text-base font-bold drop-shadow",
+            )}
+          >
+            회원가입
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
