8d3cde0bcf62700e9efab1bac06fb5dc2aaff9a8
PresentKim(김현재) <debe3721@gmail.com>
2024-08-14 12:10:41 +0900

    refactor(server/config): ConfigurationProperties 을 사용해 JWT 관련 설정을 빈에 바인딩

--- server/src/main/java/today/wtfood/server/WhatTheFoodApplication.java
+++ server/src/main/java/today/wtfood/server/WhatTheFoodApplication.java
@@ -2,8 +2,13 @@ package today.wtfood.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import today.wtfood.server.config.properties.JwtProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        JwtProperties.class,
+})
 public class WhatTheFoodApplication {
 
     public static void main(String[] args) {
