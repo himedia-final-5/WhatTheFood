2d49447549d65422ecd5d814be5bc1b4410be958
PresentKim <debe3721@gmail.com>
2024-08-15 19:20:59 +0900

    refactor(client/header): shadecn의 popover를 활용해 유저 프로필 표시 및 프로필 메뉴 구현

--- client/src/layouts/default/header/user-button/UserButton.jsx
+++ client/src/layouts/default/header/user-button/UserButton.jsx
@@ -2,32 +2,29 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useToggle } from "@reactuses/core";
 
+import ProfilePopover from "./ProfilePopover";
 import AuthModal from "./AuthModal";
-import ProfileMenu from "./ProfileMenu";
 import { useSelector } from "stores";
-import { useToggleElement } from "hooks";
 
 export default function UserButton() {
   const location = useLocation();
   const user = useSelector((state) => state.user);
-  const [submenuRef, showSubmenu, toggleSubmenu] = useToggleElement(false);
   const [showAuthForm, toggleAuthForm] = useToggle(false);
 
   useEffect(() => {
-    toggleSubmenu(false);
     toggleAuthForm(false);
-  }, [location, user, toggleSubmenu, toggleAuthForm]);
 
-  return (
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location, user]);
+
+  return user ? (
+    <ProfilePopover />
+  ) : (
     <>
-      <div
-        className="toptopprofile"
-        onClick={() => (user ? toggleSubmenu(true) : toggleAuthForm(true))}
-      >
+      <div className="toptopprofile" onClick={toggleAuthForm}>
         <img id="img" src="/images/profile.png" alt="profile" />
       </div>
       <AuthModal visible={showAuthForm} setVisible={toggleAuthForm} />
-      <ProfileMenu ref={submenuRef} className={showSubmenu ? "" : "!hidden"} />
     </>
   );
 }
