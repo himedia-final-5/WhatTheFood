b822a7e00abd2022aea5613ccdd37b804cba4f5c
PresentKim <debe3721@gmail.com>
2024-08-16 12:43:29 +0900

    feat(client/header): 스크롤을 올리면 헤더가 나타나는 기능 추가

--- client/src/layouts/default/header/Header.js
+++ client/src/layouts/default/header/Header.js
@@ -1,11 +1,44 @@
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Header.css";
 import UserButton from "./user-button/UserButton";
+import cn from "utils/cn";
 
 function Header() {
+  const headerRef = useRef(null);
+  const [headerMargin, setHeaderMargin] = useState(0);
+  const prevY = useRef(window.scrollY);
+
+  const handleScroll = () => {
+    if (!headerRef.current) return;
+
+    const y = window.scrollY;
+    const diffY = prevY.current - y;
+    if (diffY > 0) {
+      setHeaderMargin((prev) => Math.max(prev - diffY, 0));
+    } else {
+      setHeaderMargin((prev) =>
+        Math.min(prev - diffY, headerRef.current.clientHeight),
+      );
+    }
+
+    prevY.current = y;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="Header">
+    <header
+      ref={headerRef}
+      className={cn("fixed flex-col w-full z-40", "bg-neutral-100 shadow-md")}
+      style={{
+        marginTop: -headerMargin,
+      }}
+    >
       <div className="top">
         <div className="toptoplogo">
           <Link to="/">
@@ -39,7 +72,7 @@ function Header() {
           <Link to="/events">이벤트</Link>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
