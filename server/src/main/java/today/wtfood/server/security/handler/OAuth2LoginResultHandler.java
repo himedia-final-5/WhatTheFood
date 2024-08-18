334d2720e353cbc96c762c613fb93071b2c51db5
PresentKim(김현재) <debe3721@gmail.com>
2024-08-19 02:54:19 +0900

    feat(server/security): OAuth2 인증 결과를 설정된 경로로 제공

--- server/src/main/java/today/wtfood/server/security/handler/OAuth2LoginResultHandler.java
+++ server/src/main/java/today/wtfood/server/security/handler/OAuth2LoginResultHandler.java
@@ -3,11 +3,15 @@ package today.wtfood.server.security.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import today.wtfood.server.entity.Member;
+import today.wtfood.server.security.enums.TokenPurpose;
+import today.wtfood.server.security.service.JwtService;
 
 import java.io.IOException;
 
@@ -18,15 +22,31 @@ import java.io.IOException;
 @Component
 public class OAuth2LoginResultHandler implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
 
+    private final JwtService jwtService;
+    private final String responseUri;
+
+    public OAuth2LoginResultHandler(
+            @Value("${oauth2.global.response-uri}")
+            String responseUri,
+            JwtService jwtService
+    ) {
+        this.responseUri = responseUri;
+        this.jwtService = jwtService;
+    }
+
     @Override
     public void onAuthenticationSuccess(
             HttpServletRequest request,
             HttpServletResponse response,
             Authentication authentication
     ) throws IOException {
-        log.info("OAuth2 login success: {}", authentication);
-        response.sendRedirect("http://wtfood.today:3000/");
-        // TODO: 로그인 성공 후 처리
+        // 사용자 정보 가져오기
+        Member member = (Member) authentication.getPrincipal();
+
+        // JWT 토큰 생성
+        String refreshToken = jwtService.generateToken(TokenPurpose.REFRESH_TOKEN, member.getUsername(), 1000 * 60 * 5, null);
+
+        response.sendRedirect(responseUri + "?code=" + refreshToken);
     }
 
     @Override
@@ -35,9 +55,9 @@ public class OAuth2LoginResultHandler implements AuthenticationSuccessHandler, A
             HttpServletResponse response,
             AuthenticationException exception
     ) throws IOException {
-        log.error("OAuth2 login failed: ", exception);
-        response.sendRedirect("http://wtfood.today:3000/");
-        // TODO: 로그인 실패 후 처리
+        log.error("OAuth2 Login Failed : {}", exception.getMessage());
+
+        response.sendRedirect(responseUri + "?error");
     }
 
 }
