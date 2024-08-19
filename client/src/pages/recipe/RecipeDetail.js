8a3905390baf6b28f27e26c4d272a896b79d7c91
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-08-19 16:10:44 +0900

    feat:(RecipeDetail kakao remove)

--- client/src/pages/recipe/RecipeDetail.js
+++ client/src/pages/recipe/RecipeDetail.js
@@ -11,6 +11,7 @@ import Popup from "../event/PopUp";
 
 /** @type {?RecipeDetail} */
 const DEFAULT_RECIPE = null;
+
 export default function RecipeDetail() {
   const navigate = useNavigate();
   const { id } = useParams();
@@ -47,110 +48,16 @@ export default function RecipeDetail() {
   };
 
   useEffect(() => {
-    if ((!isLoading, recipe == null && recipe?.id !== id)) {
+    if (!isLoading && (recipe == null || recipe?.id !== id)) {
       fetchEvent();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
-  useEffect(() => {
-    const loadKakaoSDK = () => {
-      if (!window.Kakao) {
-        console.error("Kakao SDK is not loaded");
-        return;
-      }
-      if (!window.Kakao.isInitialized()) {
-        window.Kakao.init("1cd0714fe86698514fb7dcd40504e5bf");
-      }
-    };
-
-    const script = document.createElement("script");
-    script.src = "https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js";
-    script.integrity =
-      "sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4";
-    script.crossOrigin = "anonymous";
-    script.onload = loadKakaoSDK;
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, [id]);
-
-  const sendLinkKakaoShare = () => {
-    if (!window.Kakao || !window.Kakao.isInitialized()) {
-      console.error("Kakao SDK is not initialized");
-      return;
-    }
-
-    if (!window.Kakao.Share || !window.Kakao.Share.sendDefault) {
-      console.error("Kakao.Link.sendDefault is not available");
-      return;
-    }
-
-    //   const templateId = 110915;
-    //   // 이벤트별 첫 번째 이미지 선택 (혹은 다른 로직으로 이미지 선택 가능)
-
-    //   const templateArgs = {
-    //     title: event.title,
-    //     description: "이벤트 설명을 여기에 추가하세요",
-    //     imageUrl:
-    //       event.contentImages.length > 0
-    //         ? event.contentImages[0]
-    //         : "기본 이미지 URL",
-    //     webUrl: `http://localhost:3000/events/${event.id}`,
-    //     mobileWebUrl: `http://localhost:3000/events/${event.id}`,
-    //   };
-
-    //   window.Kakao.Share.sendCustom({
-    //     templateId: templateId,
-    //     templateArgs: templateArgs,
-    //   });
-    // };
-
-    const imageUrl =
-      recipe.finishedImages.length > 0
-        ? recipe.finishedImages[0]
-        : "기본 이미지 URL";
-
-    window.Kakao.Share.sendDefault({
-      objectType: "feed",
-      content: {
-        title: recipe.title,
-        imageUrl: imageUrl,
-        link: {
-          mobileWebUrl: `http://localhost:3000/recipes${recipe.id}`,
-          webUrl: `http://localhost:3000/recipes/${recipe.id}`,
-        },
-      },
-      social: {
-        likeCount: 777,
-        commentCount: 77,
-        sharedCount: 777,
-      },
-      buttons: [
-        {
-          title: "웹으로 보기",
-          link: {
-            mobileWebUrl: `http://localhost:3000/recipes${recipe.id}`,
-            webUrl: `http://localhost:3000/recipes${recipe.id}`,
-          },
-        },
-        {
-          title: "앱으로 보기",
-          link: {
-            mobileWebUrl: `http://localhost:3000/recipes${recipe.id}`,
-            webUrl: `http://localhost:3000/recipes${recipe.id}`,
-          },
-        },
-      ],
-    });
-  };
-
-  if (isError) {
-    toast.error("레시피 정보를 불러오는데 실패했습니다.");
-    return <div></div>;
-  }
+  // if (isError) {
+  //   toast.error("레시피 정보를 불러오는데 실패했습니다.");
+  //   return <div></div>;
+  // }
 
   return (
     recipe && (
@@ -184,12 +91,6 @@ export default function RecipeDetail() {
           </p>
           <div className="event_custom-button_total_wrap">
             <div className="event_custom-button_wrap">
-              <button
-                className="event_custom_button"
-                onClick={sendLinkKakaoShare}
-              >
-                <img src="/images/kakao.png" alt="KakaoShare" />
-              </button>
               <CopyToClipboard text={currentUrl}>
                 <button
                   type="submit"
