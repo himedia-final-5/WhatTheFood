9e7de14810566dc56af387a898c1cb7b61a5439d
PresentKim(김현재) <debe3721@gmail.com>
2024-08-24 23:51:51 +0900

    fix(client/util): Pagination의 잘못된 페이지 경계 확인 로직 수정

--- client/src/components/util/PaginationNav.jsx
+++ client/src/components/util/PaginationNav.jsx
@@ -14,21 +14,18 @@ export default function PaginationNav({ pagination, onSelectPage }) {
   const start = Math.max(0, Math.min(current - 2, pagination.totalPages - 5));
   const end = Math.min(pagination.totalPages - 1, start + 4);
 
-  const isFistPage = current === 0;
-  const isLastPage = current === pagination.totalPages - 1;
-
   return (
     <nav aria-label="pagination" className="flex justify-center mx-auto my-2">
       <ul className="flex items-center justify-center h-9 w-72 text-base">
         <li
-          role={isFistPage ? undefined : "button"}
+          role={pagination.first ? undefined : "button"}
           className={cn(
             "flex items-center justify-center h-full px-3 leading-tight rounded-l-lg border",
-            isFistPage
+            pagination.first
               ? "border-gray-300"
               : "cursor-pointer text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700",
           )}
-          onClick={() => isFistPage || onSelectPage(current - 1)}
+          onClick={() => pagination.first || onSelectPage(current - 1)}
         >
           <PaginationLeft className="w-2.5 h-2.5" />
         </li>
@@ -52,14 +49,14 @@ export default function PaginationNav({ pagination, onSelectPage }) {
           );
         })}
         <li
-          role={isLastPage ? undefined : "button"}
+          role={pagination.last ? undefined : "button"}
           className={cn(
             "flex items-center justify-center h-full px-3 leading-tight rounded-r-lg  border ",
-            isLastPage
+            pagination.last
               ? "border-gray-300 "
               : "cursor-pointer text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700",
           )}
-          onClick={() => isLastPage || onSelectPage(current + 1)}
+          onClick={() => pagination.last || onSelectPage(current + 1)}
         >
           <PaginationRight className="w-2.5 h-2.5" />
         </li>
