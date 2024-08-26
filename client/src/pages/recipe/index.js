2739d88776f372c1b3e755fe45b0f7fa1666ce61
PresentKim(김현재) <debe3721@gmail.com>
2024-08-26 12:34:38 +0900

    fix(client/recipe): 임시로 레시피 css를 전역적으로 로드

--- client/src/pages/recipe/index.js
+++ client/src/pages/recipe/index.js
@@ -1,5 +1,12 @@
 import { lazyLoading as $ } from "utils";
 
+// TODO: 레시피 CSS 파편화
+import "./RecipeList.css";
+import "./PopUp.css";
+import "./RecipeDetail.css";
+import "./RecipeFavorite.css";
+import "./RecipeUpWrite.css";
+
 export const RecipeDetail = $(() => import("./RecipeDetail"));
 export const RecipeFavorite = $(() => import("./RecipeFavorite"));
 export const RecipeList = $(() => import("./RecipeList"));
