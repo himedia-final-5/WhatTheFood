3e7509abbe6dcffe527d9fd99c806f190cfc3a27
PresentKim(김현재) <debe3721@gmail.com>
2024-08-30 23:32:22 +0900

    feat(client/component): UserFeature 컴포넌트에 예상 아이디 설정 기능 추가

--- client/src/components/util/UserFeature.jsx
+++ client/src/components/util/UserFeature.jsx
@@ -4,13 +4,18 @@ import { useSelector } from "stores";
  * @typedef {object} Props
  * @property {React.ReactNode} children 자식 노드
  * @property {React.ReactNode} alternative 대체 노드 (기본: 빈 노드)
+ * @property {number} expectId 예상 아이디 (기본: null), 예상 아이디와 현재 아이디가 일치하지 않으면 대체 노드를 반환
  */
 /**
  * @type {(props: Props) => React.JSX.Element}
  */
-export default function UserFeature({ children, alternative = <></> }) {
+export default function UserFeature({
+  children,
+  alternative = <></>,
+  expectId = null,
+}) {
   const user = useSelector((state) => state.user);
 
-  // 회원이 아닌 경우 대체 노드를 반환
-  return user ? children : alternative;
+  // 회원이 아니거나 예상 아이디와 현재 아이디가 일치하지 않으면 대체 노드를 반환
+  return user && (!expectId || user.id === expectId) ? children : alternative;
 }
