88cb5b59b03b6b9e4f4c974231873728e8617f59
김태희 <163955023+kittyyakki@users.noreply.github.com>
2024-09-03 14:15:36 +0900

    feat:(기존 찜하기쿼리 수정)
    <Recipe>에서 <RecipeSummary>로 수정

--- server/src/main/java/today/wtfood/server/repository/RecipeRepository.java
+++ server/src/main/java/today/wtfood/server/repository/RecipeRepository.java
@@ -72,13 +72,15 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long>, JpaSpecif
 
     Page<RecipeSummary> findAllByMember_Id(Long id, Pageable pageable);
 
-    List<Recipe> findByFavoriteByMembersContains(Member member);
+    List<RecipeSummary> findByFavoriteByMembersContains(Member member);
+
+    // List<Recipe> findByFavoriteByMembersContains(Member member);
 
     @Query("SELECT r.member.id AS memberId, SUM(r.viewCount) AS totalViews " +
-           "FROM Recipe r " +
-           "WHERE r.createdDate BETWEEN :startDate AND :endDate " +
-           "GROUP BY r.member.id " +
-           "ORDER BY totalViews DESC")
+            "FROM Recipe r " +
+            "WHERE r.createdDate BETWEEN :startDate AND :endDate " +
+            "GROUP BY r.member.id " +
+            "ORDER BY totalViews DESC")
     List<Map<String, Object>> findTotalViewsByMember(
             @Param("startDate")
             Timestamp startDate,
