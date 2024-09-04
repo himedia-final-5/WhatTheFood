63ddccb16ebe553b1f477a746485e399cf403a35
PresentKim(김현재) <debe3721@gmail.com>
2024-09-04 10:44:43 +0900

    feat(client/layout/fallback): ErrorRender 를 다양한 상황에 사용할 수 있도록 수정

--- client/src/layouts/fallback/ErrorRender.jsx
+++ client/src/layouts/fallback/ErrorRender.jsx
@@ -1,11 +1,54 @@
-export default function fallbackRender({ error, resetErrorBoundary }) {
-  console.error(error);
-  console.log(resetErrorBoundary);
+import { Link } from "react-router-dom";
+
+import {
+  UndrawBugFixing,
+  UndrawDonutLove,
+  UndrawServerDown,
+  UndrawSpecialEvent,
+} from "components/asset";
+import cn from "utils/cn";
+
+const getErrorImage = (status) => {
+  switch (status) {
+    case 400:
+    case 401:
+    case 403:
+      return UndrawSpecialEvent;
+    case 404:
+      return UndrawDonutLove;
+    case 504:
+      return UndrawServerDown;
+    default:
+      return UndrawBugFixing;
+  }
+};
+
+export default function ErrorRender({ error, resetErrorBoundary, ...props }) {
+  const ErrorImage = getErrorImage(
+    Number(error?.status || error?.response?.status || 500),
+  );
 
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
+    <div
+      {...props}
+      role="alert"
+      className={cn(
+        "flex flex-col items-center justify-center w-full h-full mx-10 gap-4 py-4",
+        props.className,
+      )}
+    >
+      <h1 className="text-2xl font-bold">
+        {error?.toastMessage || error?.message || "오류가 발생했습니다"}
+      </h1>
+      <ErrorImage className="w-full mb-10 xs:mb-0 xs:w-2/3 max-w-screen-xs text-primary" />
+      <div className="flex gap-4 *:w-28 *:py-1 *:rounded-lg *:text-lg *:font-bold *:text-center *:text-white">
+        <Link to="/" className="bg-primary">
+          홈으로
+        </Link>
+        <button onClick={window.location.reload} className="bg-neutral-700">
+          새로고침
+        </button>
+      </div>
     </div>
   );
 }
