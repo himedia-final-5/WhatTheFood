c9e64ae67ec634e49cafeb9d7a2ed6e23ac387ba
PresentKim(김현재) <debe3721@gmail.com>
2024-09-05 11:11:23 +0900

    fix(client): 채널톡 아이콘을 기본 레이아웃에만 적용

--- client/src/index.js
+++ client/src/index.js
@@ -10,16 +10,6 @@ import "./styles/reset.css";
 import "./styles/shadcn.css";
 import App from "./App";
 import store from "stores";
-import ChannelService from "ChannelService";
-
-ChannelService.loadScript();
-
-const options = {
-  pluginId: "01934fbe-4eab-4fab-a723-3254167cdcf0", // 채널톡 대시보드에서 제공받은 플러그인 ID
-};
-
-// 채널톡 초기화
-ChannelService.boot(options, () => {});
 
 const reportWebVitals = () => {
   import("./reportWebVitals").then((module) => module.default());
--- client/src/layouts/default/DefaultLayout.js
+++ client/src/layouts/default/DefaultLayout.js
@@ -2,6 +2,16 @@ import { Outlet } from "react-router-dom";
 
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import ChannelService from "ChannelService";
+
+ChannelService.loadScript();
+
+const options = {
+  pluginId: "01934fbe-4eab-4fab-a723-3254167cdcf0", // 채널톡 대시보드에서 제공받은 플러그인 ID
+};
+
+// 채널톡 초기화
+ChannelService.boot(options, () => {});
 
 export default function DefaultLayout() {
   return (
