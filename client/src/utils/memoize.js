6afdbc7e9a2338cebb8ebab7fa49a1e61e08b1c3
PresentKim(김현재) <debe3721@gmail.com>
2024-09-08 11:49:29 +0900

    refactor(client/util): memoize 유틸 함수에 설명 주석을 추가하고 일반 함수를 변환하지 않도록 수정

--- client/src/utils/memoize.js
+++ client/src/utils/memoize.js
@@ -1,18 +1,34 @@
+/**
+ * 메모이제이션 함수 (함수의 반환 값을 캐싱해 재사용)
+ *
+ * - 목적 1) 함수의 반환 값을 캐싱하여 성능 향상
+ * - 목적 2) 시간이 오래 걸리는 로직의 반복을 피함 (예: API 호출)
+ *
+ * @template T
+ * @param {(...args: any[]) => T} func - 메모이제이션할 함수
+ * @returns {(...args: any[]) => T} 메모이제이션된 함수
+ *
+ * @link https://en.wikipedia.org/wiki/Memoization
+ */
 export default function memoize(func) {
-  const cache = new Map();
+  const resultCacheMap = new Map();
 
-  return function (...args) {
+  return (...args) => {
+    // 캐시 키 생성
     const key = JSON.stringify(args);
-    if (cache.has(key)) {
-      return Promise.resolve(cache.get(key));
+
+    // 캐시에 값이 있으면 반환
+    if (resultCacheMap.has(key)) {
+      return resultCacheMap.get(key);
     }
 
-    const promise = func(...args).then((result) => {
-      cache.set(key, result);
-      return result;
-    });
+    // 캐시에 값이 없으면 함수 실행
+    const result = func(...args);
 
-    cache.set(key, promise);
-    return promise;
+    // 함수 실행 결과를 캐시에 저장
+    resultCacheMap.set(key, result);
+
+    // 함수 실행 결과를 반환
+    return result;
   };
 }
