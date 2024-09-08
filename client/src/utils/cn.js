030da5ea87399e42ca9f0288bd3f33cbd6789dca
PresentKim(김현재) <debe3721@gmail.com>
2024-09-08 11:23:50 +0900

    refactor(client/util): cn 유틸 함수에 설명 주석을 추가

--- client/src/utils/cn.js
+++ client/src/utils/cn.js
@@ -2,11 +2,15 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 /**
- * 주어진 클래스 네임을 Tailwind CSS 클래스로 변환합니다.
+ * 주어진 클래스 네임들을 하나의 클래스 네임으로 변환합니다.
  *
- * @param {ClassValue[]} inputs
+ * - 목적 1) 너무 긴 클래스 네임을 분리하거나 그룹 별로 묶어 가독성을 향상
+ * - 목적 2) 조건부 클래스 네임을 가독성을 해치지 않으며 적용
+ * - 목적 3) 중복되는 클래스 네임을 자동으로 제거 (tailwind-merge)
+ *
+ * @param {import("tailwind-merge").ClassNameValue[]} classLists
  * @returns {string}
  */
-export default function cn(...inputs) {
-  return twMerge(clsx(inputs));
+export default function cn(...classLists) {
+  return twMerge(clsx(classLists));
 }
