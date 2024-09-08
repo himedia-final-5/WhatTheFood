60423d4460bf0826bfa63f7a416b780110caaf8b
PresentKim(김현재) <debe3721@gmail.com>
2024-09-08 11:24:37 +0900

    refactor(client/util): lazyLoading 유틸 함수에 설명 주석을 추가

--- client/src/utils/lazyLoading.js
+++ client/src/utils/lazyLoading.js
@@ -3,6 +3,9 @@ import { lazy, Suspense } from "react";
 /**
  * 지연 로딩 컴포넌트를 `<Suspense>` 컴포넌트로 감싸서 반환
  *
+ * - 목적 1) 컴포넌트를 동적으로 불러오는 것을 지연
+ * - 목적 2) 컴포넌트가 로딩되는 동안 로딩 상태를 표시
+ *
  * @template {React.ComponentType<any>} T - 동적으로 불러올 컴포넌트의 타입
  * @param {() => Promise<{ default:  T }>} imports - 동적으로 불러올 컴포넌트
  * @param {React.ReactNode} fallback - 로딩 중 표시할 컴포넌트
